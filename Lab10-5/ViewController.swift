c032e98cb97e4f33c5633cb4f23c047e8101421a
Lucas Pillar <lucaspillar@loan-rds-468-x.local>
2015-10-05 14:51:08 -0500

    Initial Commit

---
+++ Lab10-5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab10-5
+//
+//  Created by Lucas Pillar on 10/5/15.
+//  Copyright (c) 2015 Lucas Pillar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
